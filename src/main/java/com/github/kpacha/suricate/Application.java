6f26bb4289434a2b8ed64184da8da235ca9726a7
Kpacha <kpacha666@gmail.com>
2014-03-16 16:33:43 +0100

    add an instance cleanup singleton

--- src/main/java/com/github/kpacha/suricate/Application.java
+++ src/main/java/com/github/kpacha/suricate/Application.java
@@ -2,20 +2,27 @@ package com.github.kpacha.suricate;
 
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import com.google.common.collect.Sets;
 import com.netflix.curator.framework.CuratorFramework;
 import com.netflix.curator.framework.CuratorFrameworkFactory;
 import com.netflix.curator.retry.ExponentialBackoffRetry;
+import com.netflix.curator.x.discovery.ServiceDiscovery;
 import com.netflix.curator.x.discovery.details.JsonInstanceSerializer;
 import com.netflix.curator.x.discovery.details.ServiceDiscoveryImpl;
 import com.netflix.curator.x.discovery.server.contexts.StringDiscoveryContext;
 import com.netflix.curator.x.discovery.server.entity.JsonServiceInstanceMarshaller;
 import com.netflix.curator.x.discovery.server.entity.JsonServiceInstancesMarshaller;
 import com.netflix.curator.x.discovery.server.entity.JsonServiceNamesMarshaller;
+import com.netflix.curator.x.discovery.server.rest.InstanceCleanup;
 import com.netflix.curator.x.discovery.strategies.RandomStrategy;
 
 public class Application extends javax.ws.rs.core.Application {
+    private static Logger logger = Logger.getLogger(Application.class);
+
     private StringDiscoveryContext context;
+    private InstanceCleanup instanceCleanup;
     private JsonServiceNamesMarshaller serviceNamesMarshaller = new JsonServiceNamesMarshaller();
     private JsonServiceInstanceMarshaller<String> serviceInstanceMarshaller;
     private JsonServiceInstancesMarshaller<String> serviceInstancesMarshaller;
@@ -25,16 +32,28 @@ public class Application extends javax.ws.rs.core.Application {
 		configuration.getZkConnection(), new ExponentialBackoffRetry(
 			1000, 3));
 	curatorFramework.start();
+	logger.debug("Curator framework started!");
 
-	context = new StringDiscoveryContext(new ServiceDiscoveryImpl<String>(
+	ServiceDiscovery<String> serviceDiscovery = new ServiceDiscoveryImpl<String>(
 		curatorFramework, configuration.getZkBasePath(),
-		new JsonInstanceSerializer<String>(String.class), null),
-		new RandomStrategy<String>(), 10);
+		new JsonInstanceSerializer<String>(String.class), null);
+	logger.debug("ServiceDiscovery started!");
+
+	context = new StringDiscoveryContext(serviceDiscovery,
+		new RandomStrategy<String>(),
+		configuration.getInstanceCleanupMillis());
+	logger.debug("DiscoveryContext started!");
+
+	instanceCleanup = new InstanceCleanup(serviceDiscovery,
+		configuration.getInstanceCleanupMillis());
+	instanceCleanup.start();
+	logger.debug("SuricateInstanceCleanup started!");
 
 	serviceInstanceMarshaller = new JsonServiceInstanceMarshaller<String>(
 		context);
 	serviceInstancesMarshaller = new JsonServiceInstancesMarshaller<String>(
 		context);
+	logger.debug("Json marshallers started!");
     }
 
     @Override
@@ -48,6 +67,7 @@ public class Application extends javax.ws.rs.core.Application {
     public Set<Object> getSingletons() {
 	Set<Object> singletons = Sets.newHashSet();
 	singletons.add(context);
+	singletons.add(instanceCleanup);
 	singletons.add(serviceNamesMarshaller);
 	singletons.add(serviceInstanceMarshaller);
 	singletons.add(serviceInstancesMarshaller);
